f7cee771a24435f6b6087c37a0f39c62cf6d1f22
Miguel Prada <mapra99@gmail.com>
2020-01-14 13:45:00 -0500

    create routes for travels

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@
 
 Rails.application.routes.draw do
   devise_for :travelers
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :travels, only: %i[new create]
+  root 'travels#new'
 end
