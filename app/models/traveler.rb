6078fc42f8c8c6d3150a3fe8919e5476b98c25d8
Miguel Prada <mapra99@gmail.com>
2020-01-15 15:14:32 -0500

    added validations for traveler

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -6,7 +6,10 @@ class Traveler < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :rememberable
 
-  validates :username, uniqueness: true
+  validates :username, uniqueness: true, presence: true
+  validates :password, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
 
   has_many :travelers_travels
   has_many :travels, through: :travelers_travels
