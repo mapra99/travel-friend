b7ca6bd57a2865635b47ca238c605c5e6aecfc86
Miguel Prada <mapra99@gmail.com>
2020-01-15 17:05:42 -0500

    fixed hour

--- app/controllers/travels_controller.rb
+++ app/controllers/travels_controller.rb
@@ -8,21 +8,26 @@ class TravelsController < ApplicationController
   end
 
   def create
-    travel = Travel.create
-    (0...traveler_params[:traveler_name].length).each do |i|
-      next if traveler_params[:traveler_name][i] == '0'
+    if traveler_params[:traveler_name].any?{|x| x!='0'}
+      travel = Travel.create  
+      (0...traveler_params[:traveler_name].length).each do |i|
+        next if traveler_params[:traveler_name][i] == '0'
 
-      traveler = travel.travelers_travels.build
-      traveler.traveler_id = traveler_params[:traveler_name][i]
-      traveler.auto = true unless traveler_params[:traveler_auto][i] == '0'
-      unless traveler_params[:traveler_parking][i] == '0'
-        traveler.parking_reserved = true
+        traveler = travel.travelers_travels.build
+        traveler.traveler_id = traveler_params[:traveler_name][i]
+        traveler.auto = true unless traveler_params[:traveler_auto][i] == '0'
+        unless traveler_params[:traveler_parking][i] == '0'
+          traveler.parking_reserved = true
+        end
+
+        traveler.save
       end
-
-      traveler.save
+      flash[:success] = 'Viaje Registrado'
+      redirect_to travels_path
+    else
+      flash[:alert] = "Ingrese al menos 1 viajero"
+      redirect_to new_travel_path
     end
-    flash[:success] = 'Viaje Registrado'
-    redirect_to travels_path
   end
 
   def index
